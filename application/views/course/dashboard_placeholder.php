9b8c3406f311fabbf89d9182946c01ec551ea609
Jimmy Hu <jim2212001@gmail.com>
2013-10-09 12:30:17 +0800

    Change FB Page URL

--- application/views/course/dashboard_placeholder.php
+++ application/views/course/dashboard_placeholder.php
@@ -1,2 +1,2 @@
 <h1><?=$course['CourseName'];?></h1>
-本頁面正在開發中，你可以使用<a href="/feedback/">意見回顧</a>或是PO在<a href="https://www.facebook.com/pages/NCTU-E3X/1403815759848058">FB粉絲團</a>告訴我你想要在這邊看到什麼
+本頁面正在開發中，你可以使用<a href="/feedback/">意見回顧</a>或是PO在<a href="https://www.facebook.com/NCTU.E3X">FB粉絲團</a>告訴我你想要在這邊看到什麼
